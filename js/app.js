46fd66242090c2acf694233d735ff906a17ba056
Noiziee <dima.maskva.1194@gmail.com>
2023-02-10 14:51:32 +0300

    init(activebox): add burger

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ let swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+document.querySelector('#menu__toggle').addEventListener('change', function (e) {
+  document.body.style.overflow = e.target.checked === true ? 'hidden' : '';
+});
